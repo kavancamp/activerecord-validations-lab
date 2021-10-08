147b8b505e445a8aa6039c5f1a1e7076b776c761
Keenah <kvancamp@mail.com>
2021-10-08 03:06:47 -0500

    commit tree 3d5d98c23463d9d4951b10679611956c29728e85

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    
+    validates :summary, length: { maximum: 250,
+    too_long: "%{count} characters is the maximum allowed" }
+    
+    validates :content, length: { minimum: 250,
+    too_short: "%{count} characters is the minimum allowed" }
+
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
+
+    validate :clickbait
+
+   def clickbait
+
+        if title.present? && !title.include?("Won't Believe")
+            errors.add(:title, "Not clickbait-y")
+        end
+   end
 end
